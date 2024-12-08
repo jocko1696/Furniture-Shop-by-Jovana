6491ee8fec9baf2cb429b6e8f8e58b8baa8d0efe
jocko1696 <joca.gluhovic@gmail.com>
2024-12-08 21:49:54 +0100

    Contact page image

--- furniture-shop/src/components/ContactComponent.jsx
+++ furniture-shop/src/components/ContactComponent.jsx
@@ -15,12 +15,11 @@ const Contact = () => {
             <div className="contact-container centerContainer">
                 <section className="contact-section flex gap-[30px] infoAboutUs">
                     <div className="w-1/2 half">
-                        <h2>Information About Us</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque
-                            ultrices mattis aliquam, malesuada diam est. Malesuada sem tristique amet
-                            erat vitae eget dolor lobortis. Accumsan faucibus vitae lobortis quis
-                            bibendum quam.
+                        <h2 className="infoHeader sm:text-center">Information About Us</h2>
+                        <p className="text-justify">
+                            We are dedicated to providing exceptional products and services that cater to your needs and exceed expectations.
+                            With a passion for quality and innovation, our team works tirelessly to deliver value and build lasting relationships.
+                            At the heart of our mission is a commitment to customer satisfaction and a focus on making a positive impact.
                         </p>
                         <div className="icon-row">
                             <div className="icon purple"></div>
@@ -28,10 +27,10 @@ const Contact = () => {
                             <div className="icon blue"></div>
                         </div></div>
                     <div className="contact-section half w-1/2 gap-5">
-                        <h2>Contact Way</h2>
-                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-14">
-                            <div className="flex w-full justify-between">
-                                <div className="contact-way w-1/2">
+                        <h2 className="infoHeader sm:text-center">Contact Way</h2>
+                        <div className="grid grid-cols-1 gridContactWay lg:grid-cols-2 gap-14">
+                            <div className="flex w-full justify-between sm:flex-col sm:justify-center sm:items-center sm:gap-[30px]">
+                                <div className="contact-way w-1/2 sm:w-full">
                                     <div className="icon purple min-w-[45px] min-h-[45px]"></div>
                                     <div className="flex flex-col my-auto">
                                         <p className="flex">
@@ -48,19 +47,19 @@ const Contact = () => {
                                         </p>
                                     </div>
                                 </div>
-                                <div className="contact-way w-1/2">
+                                <div className="contact-way w-1/2 sm:w-full">
                                     <div className="icon pink min-w-[45px] min-h-[45px]"></div>
                                     <p>Support Forum For Over 24 Hours</p>
                                 </div>
                             </div>
-                            <div className="flex w-full justify-between">
-                                <div className="contact-way w-1/2">
+                            <div className="flex w-full justify-between sm:flex-col sm:justify-center sm:items-center sm:gap-[30px]">
+                                <div className="contact-way w-1/2 sm:w-full">
                                     <div className="icon yellow min-w-[45px] min-h-[45px]"></div>
                                     <p className="flex">
                                         On Your Heart <FaHeart style={{ color: "red" }} className="ml-1.5 mt-[3px]" />
                                     </p>
                                 </div>
-                                <div className="contact-way w-1/2">
+                                <div className="contact-way w-1/2 sm:w-full">
                                     <div className="icon green min-w-[45px] min-h-[45px]"></div>
                                     <p>Free Standard Shipping On All Orders</p>
                                 </div>
@@ -70,17 +69,18 @@ const Contact = () => {
                     </div>
                 </section>
 
-                <section className="contact-section flex">
-                    <div className="w-1/2">
-                        <h2>Get In Touch</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque
-                            ultrices tristique amet erat vitae eget dolor los vitae lobortis quis
-                            bibendum quam.
+                <section className="contact-section flex sm:flex-col">
+                    <div className="w-1/2 sm:w-full">
+                        <h2 className="infoHeader">Get In Touch</h2>
+                        <p className="infoDesc text-justify">
+                            Have questions or need assistance? Our team is here to help with expert guidance and personalized support.
+                            Whether you’re looking for more information, have a specific request, or just want to say hello, we’d love to hear from you.
+                            Reach out to us today and experience our commitment to exceptional service.
+                            Your inquiries are important to us, and we’ll get back to you as quickly as possible.
                         </p>
                         <ContactUs/>
                     </div>
-                    <div className="contact-section w-1/2">
+                    <div className="contact-section w-1/2 sm:w-full">
                         <img src={ContactUsIMG} alt="Contact Us" className="contact-image" />
                     </div>
                 </section>
